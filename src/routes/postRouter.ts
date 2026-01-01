e1b97267902061317cba1a5d7302e92f220b715a
noya <noyam1003@gmail.com>
2026-01-01 15:06:24 +0200

    added post model and router

---
+++ src/routes/postRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+export const postRouter = express.Router();
+
+postRouter.get("/post", );
+
+postRouter.get("post/:id", );
+
+postRouter.post("/post", );
+
+postRouter.delete("post/:id", );
+
+postRouter.put("post/:id", );
