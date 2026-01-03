e1bf50247ef5f4921731e081338f3db645244a49
noya <noyam1003@gmail.com>
2026-01-03 17:04:44 +0200

    deleted dist

--- dist/src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const PORT = process.env.PORT || 3000;
-(0, index_1.initApp)().then((app) => {
-    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
-});
-//# sourceMappingURL=server.js.map
