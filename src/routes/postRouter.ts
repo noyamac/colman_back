b3e0c7cdadd5e49dbd817423470ec3c7a69cd1c2
noya <noyam1003@gmail.com>
2026-01-23 14:43:41 +0200

    added user crud router, controller, req

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import postController from "../controllers/postController";
+import express from 'express';
+import postController from '../controllers/postController';
 
 export const postRouter = express.Router();
 
-postRouter.get("/post", postController.getAll.bind(postController));
+postRouter.get('/post', postController.getAll.bind(postController));
 
-postRouter.get("/post/:id", postController.getById.bind(postController));
+postRouter.get('/post/:id', postController.getById.bind(postController));
 
-postRouter.post("/post", postController.create.bind(postController));
+postRouter.post('/post', postController.create.bind(postController));
 
-postRouter.delete("/post/:id", postController.delete.bind(postController));
+postRouter.delete('/post/:id', postController.delete.bind(postController));
 
-postRouter.put("/post/:id", postController.update.bind(postController));
+postRouter.put('/post/:id', postController.update.bind(postController));
